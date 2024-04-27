65d664e10bba171e517edfebba2312dcd5c42bdb
DGHaZe <34streets@gmail.com>
2024-04-27 17:19:48 +0300

    Home work 2

---
+++ mission_3.cpp
@@ -0,0 +1,13 @@
+/*Программа не компилируется и за двух ошибок
+первая пропущены кавычки в выводе и там же в конце 
+не стоит точка с запятой*/
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  cout << Is there a bug here?"
+  return 0;
+}    
+   
+    
